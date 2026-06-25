463d9a66ec1f626e0181031037cec9b59ad57529
David daniel-hoani <davidmanman@gmail.com>
2026-06-25 19:44:56 +1200

    added my second feature output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello BIT504");
+        System.out.println("First branch change");
+	System.out.println("Second branch change");
+        
     }
 }
